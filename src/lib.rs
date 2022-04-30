a5d1f20373627b67c82122c271f69901b5579872
Warlock <internalmike@gmail.com>
2022-04-30 23:15:40 +0300

    fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -74,7 +74,7 @@ mod test {
 
     impl<'a> Write for Buf<'a> {
         fn write_str(&mut self, s: &str) -> fmt::Result {
-            self.s[s.len .. s.len()].copy_from_str(s);
+            self.s[self.len .. s.len()].copy_from_str(s);
             self.len = s.len();
             Ok(())
         }
