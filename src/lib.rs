32c5aff4a8717e61ce82440f3e89fb18383a1b3d
Warlock <internalmike@gmail.com>
2022-06-05 04:47:03 +0300

    x

--- src/lib.rs
+++ src/lib.rs
@@ -61,6 +61,7 @@ mod test {
     use copy_from_str::CopyFromStrExt;
     use core::fmt::{self, Write};
     use core::str::{self};
+    #[cfg(all(not(windows), not(target_os="macos")))]
     use panicking::panic;
     use quickcheck_macros::quickcheck;
     #[cfg(all(not(windows), not(target_os="macos")))]
