7c8acdbf4582f1353cc38eecdcd447b99d66e513
Warlock <internalmike@gmail.com>
2023-10-24 05:17:29 +0300

    ens

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -1,55 +1,31 @@
-#![feature(default_alloc_error_handler)]
-#![feature(lang_items)]
 #![feature(start)]
 
 #![deny(warnings)]
 
 #![no_std]
 
-use core::alloc::Layout;
-use core::panic::PanicInfo;
-use errno_no_std::*;
-#[cfg(not(windows))]
-use libc::EINVAL;
-#[cfg(not(windows))]
-use libc::exit;
-use libc_alloc::LibcAlloc;
-#[cfg(windows)]
-use winapi::shared::minwindef::UINT;
-#[cfg(windows)]
-use winapi::um::processthreadsapi::ExitProcess;
-#[cfg(windows)]
-use winapi::shared::winerror::ERROR_ACCESS_DENIED;
-
 #[cfg(windows)]
 #[link(name="msvcrt")]
 extern { }
 
-#[global_allocator]
-static ALLOCATOR: LibcAlloc = LibcAlloc;
+mod no_std {
+    use core::panic::PanicInfo;
+    use exit_no_std::exit;
 
+    #[panic_handler]
+    fn panic(_info: &PanicInfo) -> ! {
+        exit(99)
+    }
+
+    #[no_mangle]
+    extern fn rust_eh_personality() { }
+}
+
+use errno_no_std::{Errno, errno, set_errno};
+#[cfg(not(windows))]
+use libc::EINVAL;
 #[cfg(windows)]
-unsafe fn exit(code: UINT) -> ! {
-    ExitProcess(code);
-    loop { }
-}
-
-#[panic_handler]
-pub extern fn panic(_info: &PanicInfo) -> ! {
-    unsafe { exit(99) }
-}
-
-#[no_mangle]
-pub fn rust_oom(_layout: Layout) -> ! {
-    unsafe { exit(98) }
-}
-
-#[lang="eh_personality"]
-extern fn rust_eh_personality() { }
-#[no_mangle]
-pub extern fn rust_eh_register_frames() { }
-#[no_mangle]
-pub extern fn rust_eh_unregister_frames() { }
+use winapi::shared::winerror::ERROR_ACCESS_DENIED;
 
 #[cfg(not(windows))]
 const ERR: i32 = EINVAL;
